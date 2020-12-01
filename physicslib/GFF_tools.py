6bc51536dae7aee850d4d420e0b019f8cad5a9cd
cha78317 <aidan.charles@lumentum.com>
2020-12-01 14:31:28 -0500

    Revert "Completed GFF tools"
    This reverts commit a6f6f227adbf1e55a7917d7c2dd2a7726061add8.

--- physicslib/GFF_tools.py
+++ physicslib/GFF_tools.py
@@ -15,47 +15,41 @@ def Get_Excel_file(gff_type):
     return data 
     
 #data = Get_Excel_file('GFF2')
-def closest_index(WL_list, K): 
-    Closest = WL_list[min(range(len(WL_list)), key = lambda i: abs(WL_list[i]-K))] 
-    return list(WL_list).index(Closest)
-
-def Put_zeros(Loss_list, index, direction):
-    if direction == "start": 
-        i = 0 
-        while i < index+1: 
-            Loss_list[i]= 0 
-            i += 1    
-    if direction == "end": 
-        i = len(Loss_list)-1 
-        while i > index-2: 
-            Loss_list[i]= 0 
-            i -= 1      
-    return Loss_list
 
 def Load_GFF_data(WL_sig, WL_pump, WL_ase,gff_type):
     #file = r"gff\GFF2.xlsx"   # Will be remplaced by Get_Excel_file: data = Get_Excel_file(GFF2)
     data =  Get_Excel_file(gff_type)# pd.read_excel(file,sheet_name='Insertion_Loss')
     Wavelength  = np.squeeze( data[['Wavelength']] )
     Loss = np.squeeze(data[['IL']] )
-    Loss_interpolation = interp1d(Wavelength, Loss, kind='cubic', fill_value="extrapolate")
+    Loss_interpolation = interp1d(Wavelength, Loss, kind='linear', fill_value="extrapolate")
     Loss_sig_raw =  Loss_interpolation(WL_sig)
-    Loss_pump =  Loss_interpolation(WL_pump)
+    Loss_pump_raw =  Loss_interpolation(WL_pump)
     Loss_ase_raw =  Loss_interpolation(WL_ase)
 
-    closest_index_ase_start = closest_index(WL_ase,Wavelength[0])
-    closest_index_ase_end = closest_index(WL_ase,Wavelength[len(Wavelength)-1])
-    closest_index_sig_start = closest_index(WL_sig,Wavelength[0])
-    closest_index_sig_end = closest_index(WL_sig,Wavelength[len(Wavelength)-1])
+    # Put zero all the entries that are not availible in the excel sheet
+    Loss_sig=[0]*len(WL_sig)
+    Loss_pump=[0]*len(WL_pump)
+    Loss_ase=[0]*len(WL_ase)
 
-    Loss_ase = Put_zeros(Loss_ase_raw, closest_index_ase_start, "start")
-    Loss_ase = Put_zeros(Loss_ase, closest_index_ase_end, "end")
-    Loss_sig = Put_zeros(Loss_sig_raw, closest_index_sig_start, "start")
-    Loss_sig = Put_zeros(Loss_sig, closest_index_sig_end, "end")  
-    Loss_pump[0] = 0 
-    
-    Total_loss = np.concatenate((Loss_sig,Loss_pump,Loss_ase))
+    for i in range(len(Wavelength)) :#this solution works but its a O(N^2) so I can make it more efficient if its too so 
+        for j in range (len(WL_sig)):
+            if int(Wavelength[i])==int(WL_sig[j]):#did the int stuff to avoid situation where both are doubles that are the same with diffrent decimal values
+                Loss_sig[j]=Loss_sig_raw[i]
+                break
+            if int(Wavelength[i])>int(WL_sig[len(WL_sig)-1]):
+                break
+        for j in range (len(WL_pump)):
+            if int(Wavelength[i])==int(WL_pump[j]):#did the int stuff to avoid situation where both are doubles that are the same with diffrent decimal values
+                Loss_pump[j]=Loss_pump_raw[i]
+                break
+            if int(Wavelength[i])>int(WL_pump[len(WL_pump)-1]):
+                break
+        for j in range (len(WL_ase)):
+            if int(Wavelength[i])==int(WL_ase[j]):#did the int stuff to avoid situation where both are doubles that are the same with diffrent decimal values
+                Loss_ase[j]=Loss_ase_raw[i]
+                break
+            if int(Wavelength[i])>int(WL_ase[len(WL_ase)-1]):
+                break
+           
 
-    return Total_loss
-
-#a = Load_GFF_data(WL_sig, WL_pump, WL_ase,'GFF2') 
-
+a = Load_GFF_data(WL_sig, WL_pump, WL_ase,'GFF2') 
--- physicslib/gff_physics.py
+++ physicslib/gff_physics.py
@@ -1,16 +1,9 @@
 import numpy as np
 import edfacomponent.gff as gffobj
-import Settings
-from physicslib.GFF_tools import Load_GFF_data
 
 def gff_physics(gffobj):
     Loss_dB = gffobj.getGffLoss()
     Loss    = np.power(10 , -Loss_dB/10 )
-    gff_Loss = Load_GFF_data(Settings.WL_sig, Settings.WL_pump, Settings.WL_ase,'GFF2') 
-
-    # Gff loss might also need to be converted from dB to linear before multiplying 
-    # Need to fix GFF loss shape
-    # Both regular loss and GFF loss should be added at this stage 
     
     InputForward    = gffobj.getInputForward()
     InputBackward   = gffobj.getInputBackward()
