f373489a39cb0095709f0aa353af7241369b161f
Amina Berrada <Amina.Berrada@lumentum.com>
2020-12-08 15:19:34 -0500

    Finalized wavelength dependency for GFF

--- physicslib/gff_physics.py
+++ physicslib/gff_physics.py
@@ -6,17 +6,21 @@ from physicslib.GFF_tools import Load_GFF_data
 def gff_physics(gffobj):
     Loss_dB = gffobj.getGffLoss()
     Loss    = np.power(10 , -Loss_dB/10 )
-    gff_Loss = Load_GFF_data(Settings.WL_sig, Settings.WL_pump, Settings.WL_ase,'GFF2') 
-
+    gff_Loss_db = Load_GFF_data(Settings.WL_sig, Settings.WL_pump, Settings.WL_ase,gffobj.component_name) 
+    gff_Loss    = np.power(10 , np.negative(gff_Loss_db)/10 )
     # Gff loss might also need to be converted from dB to linear before multiplying 
     # Need to fix GFF loss shape
     # Both regular loss and GFF loss should be added at this stage 
     
     InputForward    = gffobj.getInputForward()
     InputBackward   = gffobj.getInputBackward()
-    OutputForward   = np.multiply(Loss,InputForward)
+
+    OutputForward_interim   = np.multiply(Loss,InputForward)
     OutputBackward  = np.multiply(Loss,InputBackward)
- 
+
+    OutputForward = np.multiply(gff_Loss,OutputForward_interim)
+    #OutputBackward  = np.multiply(gff_Loss,OutputBackward_interim)
+
     gffobj.setOutputForward(OutputForward)
     gffobj.setOutputBackward(OutputBackward)
   
